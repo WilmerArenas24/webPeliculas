75da5c44fd32b55b466d86d837dccdb1caa260fe
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-04-06 21:19:20 -0500

    Creando el listado de peliculas

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
+
+  const [listadoState, setListadoState] = useState([]);
+  
+  useEffect(() =>{
+    console.log('Componentes de peliculas cargado!')
+       conseguirPeliculas();
+  }, []);
+
+  //Creando un metodo para conseguir las peliculas
+  const conseguirPeliculas = () =>{
+    //metodo para convertir lo que trae por llaves a que sea utilizable en javascript
+    let peliculas = JSON.parse(localStorage.getItem("pelis"));
+    
+    //Rellenar con las peliculas
+    setListadoState(peliculas);
+  }
+
   return (
     <>
-        {[1, 2, 3, 4].map(index => (
-          <article key={index} className="peli-item">
-            <h3 className="title">Desarrollo web</h3>
-            <p className="description">Wilmerarenas.es</p>
+    {/* Recorriendo todas las peliculas que traigo por array */}
+        {listadoState != null ?
+                listadoState.map(peli =>{
+                  return (
+                    <article key={peli.id} className="peli-item">
+                        <h3 className="title">{peli.titulo}</h3>
+                        <p className="description">{peli.descripcion}</p>
 
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-          </article>
-        ))}
+                        <button className="edit">Editar</button>
+                        <button className="delete">Borrar</button>
+                  </article>
+                  );
+                })
+        : <h2>No existen peliculas aquí</h2>        
+      }
+
     </>
   )
 }
